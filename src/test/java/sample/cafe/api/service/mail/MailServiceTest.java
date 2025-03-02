a5ed81864e83d6f0c4dc3e6f0f4aae4db9572aeb
PeachCoolpis <rlglxh@naver.com>
2025-03-03 08:00:42 +0900

    feat. BDDMockito

--- src/test/java/sample/cafe/api/service/mail/MailServiceTest.java
+++ src/test/java/sample/cafe/api/service/mail/MailServiceTest.java
@@ -1,19 +1,17 @@
 package sample.cafe.api.service.mail;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import sample.cafe.api.client.MailSendClient;
 import sample.cafe.domain.history.mail.MailSendHistory;
 import sample.cafe.domain.history.mail.MailSendHistoryRepository;
 
-import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -32,13 +30,12 @@ class MailServiceTest {
     @Test
     void sendMail(){
         //given
-        // MailSendClient mailSendClient = mock(MailSendClient.class);
-        // MailSendHistoryRepository mailSendHistoryRepository = mock(MailSendHistoryRepository.class);
-        
-        
-        //when
         when(mailSendClient.sendEmail(anyString(), anyString(), anyString(), anyString()))
                 .thenReturn(true);
+        BDDMockito.given(mailSendClient.sendEmail(anyString(), anyString(), anyString(), anyString()))
+                .willReturn(true);
+        
+        //when
         
         boolean result = mailService.sendMail(
                 "",
